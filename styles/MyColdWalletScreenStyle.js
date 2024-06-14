767f049d113c49cfa743c97874a577ca0f3296e4
Jaime Lee <18038163520@163.com>
2024-06-14 11:24:40 +0800

    fix

---
+++ styles/MyColdWalletScreenStyle.js
@@ -0,0 +1,5 @@
+import { StyleSheet } from "react-native";
+
+export const MyColdWalletScreenStyle = StyleSheet.create({
+  // 更多的样式定义...
+});
---
+++ styles/TransactionsScreenStyle.js
@@ -0,0 +1,5 @@
+import { StyleSheet } from "react-native";
+
+export const TransactionsScreenStyle = StyleSheet.create({
+  // 更多的样式定义...
+});
