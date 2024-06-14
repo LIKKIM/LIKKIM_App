2f5ff733135ae7a6a4378bbd1137c497d4864325
Jaime Lee <18038163520@163.com>
2024-06-14 14:42:19 +0800

    Update CryptoContext.js

--- components/CryptoContext.js
+++ components/CryptoContext.js
@@ -1,12 +1,16 @@
+// CryptoContext.js
 import React, { createContext, useState } from "react";
 
 export const CryptoContext = createContext();
 
 export const CryptoProvider = ({ children }) => {
   const [cryptoCount, setCryptoCount] = useState(0);
+  const [isDarkMode, setIsDarkMode] = useState(true);
 
   return (
-    <CryptoContext.Provider value={{ cryptoCount, setCryptoCount }}>
+    <CryptoContext.Provider
+      value={{ cryptoCount, setCryptoCount, isDarkMode, setIsDarkMode }}
+    >
       {children}
     </CryptoContext.Provider>
   );
