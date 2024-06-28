3bffdb7f4f5e8088ea3ff6e7fc977120fa0176eb
Jaime Lee <18038163520@163.com>
2024-06-29 01:12:26 +0800

    Update CryptoContext.js

--- components/CryptoContext.js
+++ components/CryptoContext.js
@@ -51,7 +51,7 @@ const initialAdditionalCryptos = [
   {
     name: "Bitcoin",
     shortName: "BTC",
-    balance: "0.0",
+    balance: "100.0",
     icon: require("../assets/BitcoinIcon.png"),
     cardImage: require("../assets/Card3.png"),
     address: "1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa",
