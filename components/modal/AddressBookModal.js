4cd23e9c73d61212893adb08e5621453196771db
Jaime Lee <18038163520@163.com>
2024-08-26 19:08:05 +0800

    Update AddressBookModal.js

--- components/modal/AddressBookModal.js
+++ components/modal/AddressBookModal.js
@@ -1,7 +1,15 @@
 // AddressBookModal.js
-import React from "react";
-import { Modal, View, Text, TouchableOpacity, FlatList } from "react-native";
+import React, { useState } from "react";
+import {
+  Modal,
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  TextInput,
+} from "react-native";
 import { BlurView } from "expo-blur";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
 function AddressBookModal({
   visible,
@@ -10,8 +18,16 @@ function AddressBookModal({
   onSelect,
   styles,
   isDarkMode,
-  onAddAddress, // 添加回调函数
+  onAddAddress,
 }) {
+  const [searchAddress, setSearchAddress] = useState("");
+
+  const filteredAddresses = addresses.filter(
+    (address) =>
+      address.name.toLowerCase().includes(searchAddress.toLowerCase()) ||
+      address.address.toLowerCase().includes(searchAddress.toLowerCase())
+  );
+
   return (
     <Modal
       animationType="slide"
@@ -23,8 +39,20 @@ function AddressBookModal({
         <View style={styles.modalView}>
           <Text style={styles.modalTitle}>Address Book</Text>
 
+          {/* Search Box */}
+          <View style={styles.searchContainer}>
+            <Icon name="search" size={20} style={styles.searchIcon} />
+            <TextInput
+              style={styles.searchInput}
+              placeholder="Search Address"
+              placeholderTextColor={isDarkMode ? "#ccc" : "#666"}
+              onChangeText={setSearchAddress}
+              value={searchAddress}
+            />
+          </View>
+
           <FlatList
-            data={addresses}
+            data={filteredAddresses}
             keyExtractor={(item) => item.id}
             renderItem={({ item }) => (
               <TouchableOpacity onPress={() => onSelect(item)}>
@@ -34,14 +62,10 @@ function AddressBookModal({
               </TouchableOpacity>
             )}
           />
-          {/* "Add Address" 按钮 */}
-          <TouchableOpacity
-            onPress={onAddAddress} // 触发回调函数
-            style={styles.submitButton} // 使用现有的样式
-          >
+          {/* "Add Address" Button */}
+          <TouchableOpacity onPress={onAddAddress} style={styles.submitButton}>
             <Text style={styles.submitButtonText}>Add Address</Text>
           </TouchableOpacity>
-
           <TouchableOpacity onPress={onClose} style={styles.closeButton}>
             <Text style={styles.cancelButtonText}>Close</Text>
           </TouchableOpacity>
