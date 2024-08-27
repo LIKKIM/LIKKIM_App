43cc3826690cb722c3f9045a14bd2963a78c5c16
Jaime Lee <18038163520@163.com>
2024-08-27 15:04:42 +0800

    Update AddressBookModal.js

--- components/modal/AddressBookModal.js
+++ components/modal/AddressBookModal.js
@@ -105,10 +105,12 @@ function AddressBookModal({
               { justifyContent: "space-between" },
             ]}
           >
+            {!isAddingAddress && (
+              <Text style={styles.modalTitle}>Address Book</Text>
+            )}
+
             {!isAddingAddress ? (
               <>
-                <Text style={styles.modalTitle}>Address Book</Text>
-
                 <>
                   {/* 搜索框 */}
                   <View style={styles.searchContainer}>
@@ -142,7 +144,7 @@ function AddressBookModal({
                         >
                           <View style={{ flexDirection: "column" }}>
                             <View style={{ flexDirection: "row" }}>
-                              <Text style={styles.Text}>
+                              <Text style={[styles.Text, { marginRight: 10 }]}>
                                 Network: {item.network}
                               </Text>
                               <Text style={styles.Text}>Name: {item.name}</Text>
@@ -206,6 +208,7 @@ function AddressBookModal({
               </>
             ) : (
               <>
+                {/* 不显示 Address Book 标题时的页面 */}
                 <Text style={styles.modalTitle}>Add New Address</Text>
 
                 {/* 包裹三个输入框的视图 */}
