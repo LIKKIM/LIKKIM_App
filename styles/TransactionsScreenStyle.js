004fe6454e319e65d00018cf0fa93bdeeeec81ee
Jaime Lee <18038163520@163.com>
2024-09-03 12:09:42 +0800

    1

--- styles/TransactionsScreenStyle.js
+++ styles/TransactionsScreenStyle.js
@@ -2,7 +2,6 @@
 import { StyleSheet } from "react-native";
 
 const TransactionsScreenStyles = (isDarkMode) => {
-  // 颜色定义
   const titleColor = isDarkMode ? "#fff" : "#000";
   const textColor = isDarkMode ? "#fff" : "#000";
   const textBtnColor = isDarkMode ? "#fff" : "#fff";
@@ -18,12 +17,10 @@ const TransactionsScreenStyles = (isDarkMode) => {
     : "#FFFFFF80";
   const secondTextColor = isDarkMode ? "#ddd" : "#676776";
   const secondBtnTextColor = isDarkMode ? "#ddd" : "#e0e0e0";
-
   return StyleSheet.create({
-    // 按钮样式
     cancelAddressButton: {
-      borderWidth: 3,
-      borderColor: buttonBackgroundColor,
+      borderWidth: 3, // 设置边框宽度
+      borderColor: buttonBackgroundColor, // 设置边框颜色
       padding: 10,
       width: "100%",
       justifyContent: "center",
@@ -41,70 +38,6 @@ const TransactionsScreenStyles = (isDarkMode) => {
       alignItems: "center",
       marginBottom: 16,
     },
-    disconnectButton: {
-      marginLeft: 10,
-      paddingVertical: 5,
-      paddingHorizontal: 10,
-      backgroundColor: "#6C6CF4",
-      borderRadius: 5,
-    },
-    cancelButton: {
-      borderWidth: 3,
-      borderColor: buttonBackgroundColor,
-      padding: 10,
-      width: "100%",
-      justifyContent: "center",
-      borderRadius: 30,
-      height: 60,
-      alignItems: "center",
-    },
-    submitButton: {
-      backgroundColor: btnColor,
-      padding: 10,
-      width: "100%",
-      justifyContent: "center",
-      borderRadius: 30,
-      height: 60,
-      alignItems: "center",
-      marginBottom: 20,
-    },
-    roundButton: {
-      borderWidth: 2,
-      borderColor: btnBorderColor,
-      paddingVertical: 10,
-      paddingHorizontal: 20,
-      width: 170,
-      height: 170,
-      borderRadius: 20,
-      alignItems: "center",
-      justifyContent: "center",
-      marginBottom: 20,
-    },
-
-    // 按钮文本样式
-    submitButtonText: {
-      color: textColor,
-      fontSize: 16,
-    },
-    cancelButtonText: {
-      color: secondTextColor,
-      fontSize: 16,
-    },
-    BluetoothBtnText: {
-      color: "#fff",
-      fontSize: 16,
-    },
-    buttonText: {
-      color: textBtnColor,
-      fontSize: 16,
-      fontWeight: "bold",
-      marginBottom: 2,
-    },
-    optionButtonText: {
-      color: textBtnColor,
-    },
-
-    // 模态框样式
     blurBackground: {
       position: "absolute",
       top: 0,
@@ -113,6 +46,7 @@ const TransactionsScreenStyles = (isDarkMode) => {
       bottom: 0,
       borderRadius: 10,
     },
+
     cardContainer: {
       backgroundColor: modalBackgroundColor,
       borderRadius: 20,
@@ -120,6 +54,7 @@ const TransactionsScreenStyles = (isDarkMode) => {
       alignItems: "center",
       width: "90%",
     },
+
     pendingModalView: {
       margin: 20,
       width: "90%",
@@ -130,6 +65,41 @@ const TransactionsScreenStyles = (isDarkMode) => {
       alignItems: "center",
       justifyContent: "space-between",
     },
+    disconnectButton: {
+      marginLeft: 10,
+      paddingVertical: 5,
+      paddingHorizontal: 10,
+      backgroundColor: "#6C6CF4",
+      borderRadius: 5,
+    },
+
+    disconnectButtonText: {
+      color: "#FFFFFF",
+      fontWeight: "bold",
+    },
+
+    submitButton: {
+      backgroundColor: btnColor,
+      padding: 10,
+      width: "100%",
+      justifyContent: "center",
+      borderRadius: 30,
+      height: 60,
+      alignItems: "center",
+      marginBottom: 20,
+    },
+    submitButtonText: {
+      color: textColor,
+      fontSize: 16,
+    },
+
+    pinModalTitle: {
+      color: textColor,
+      fontSize: 20,
+      fontWeight: "bold",
+      marginBottom: 15,
+    },
+
     pinModalView: {
       position: "absolute",
       top: 100,
@@ -142,73 +112,20 @@ const TransactionsScreenStyles = (isDarkMode) => {
       justifyContent: "space-between",
       alignItems: "center",
     },
-    modalView: {
-      margin: 20,
-      height: 500,
-      width: "90%",
-      backgroundColor: modalBackgroundColor,
-      borderRadius: 20,
-      padding: 35,
-      alignItems: "center",
-      justifyContent: "space-between",
-    },
-    inputModelView: {
-      margin: 20,
-      height: 400,
-      width: "90%",
-      backgroundColor: modalBackgroundColor,
-      borderRadius: 20,
-      padding: 35,
-      alignItems: "center",
-    },
-    confirmModalView: {
-      margin: 20,
-      alignSelf: "center",
-      maxHeight: "90%",
-      width: "90%",
-      backgroundColor: modalBackgroundColor,
-      borderRadius: 20,
-      padding: 35,
-      alignItems: "center",
-      justifyContent: "space-between",
-    },
-    receiveModalView: {
-      margin: 20,
-      height: 600,
-      width: "90%",
-      backgroundColor: modalBackgroundColor,
-      borderRadius: 20,
-      padding: 35,
-      justifyContent: "space-between",
-      alignItems: "center",
-    },
-    bluetoothModalView: {
-      margin: 20,
-      height: 500,
-      width: "90%",
-      backgroundColor: modalBackgroundColor,
-      borderRadius: 20,
-      padding: 35,
-      justifyContent: "space-between",
-      alignItems: "center",
-    },
-
-    // 文本样式
-    pinModalTitle: {
+    passwordInput: {
+      backgroundColor: inputBackgroundColor,
+      paddingHorizontal: 15,
+      paddingVertical: 10,
+      borderRadius: 10,
+      height: 50,
+      width: "100%",
       color: textColor,
-      fontSize: 20,
-      fontWeight: "bold",
-      marginBottom: 15,
     },
-    modalTitle: {
-      color: titleColor,
-      fontSize: 16,
-      fontWeight: "bold",
-    },
-    modalReceiveTitle: {
-      color: titleColor,
-      fontSize: 16,
-      fontWeight: "bold",
+    deviceItemContainer: {
+      flexDirection: "row", // 横向排列
+      alignItems: "center", // 垂直居中
+      justifyContent: "center",
+      marginTop: 20,
     },
     modalSubtitle: {
       color: secondTextColor,
@@ -231,6 +148,50 @@ const TransactionsScreenStyles = (isDarkMode) => {
       fontSize: 14,
       marginBottom: 6,
     },
+    cancelButtonLookingFor: {
+      borderColor: buttonBackgroundColor,
+      borderWidth: 3,
+      padding: 10,
+      width: "100%",
+      justifyContent: "center",
+      borderRadius: 30,
+      height: 60,
+      alignItems: "center",
+      marginTop: 20,
+    },
+    cancelButtonText: {
+      color: secondTextColor,
+      fontSize: 16,
+    },
+    scanModalSubtitle: {
+      color: secondTextColor,
+      fontSize: 14,
+    },
+    bluetoothModalView: {
+      margin: 20,
+      height: 500,
+      width: "90%",
+      backgroundColor: modalBackgroundColor,
+      borderRadius: 20,
+      padding: 35,
+      justifyContent: "space-between",
+      alignItems: "center",
+    },
+    bluetoothImg: {
+      width: 150,
+      height: 150,
+      marginBottom: 30,
+    },
+    BluetoothBtnText: {
+      color: "#fff",
+      fontSize: 16,
+    },
+    bluetoothModalTitle: {
+      color: textColor,
+      fontSize: 20,
+      fontWeight: "bold",
+      marginBottom: 10,
+    },
     subtitleText: {
       fontSize: 14,
       color: secondTextColor,
@@ -243,11 +204,59 @@ const TransactionsScreenStyles = (isDarkMode) => {
       color: secondTextColor,
       fontSize: 16,
     },
-    transactionText: {
-      color: secondTextColor,
-      fontSize: 16,
-      marginBottom: 10,
+    cancelButton: {
+      borderWidth: 3, // 设置边框宽度
+      borderColor: buttonBackgroundColor, // 设置边框颜色
+      padding: 10,
+      width: "100%",
+      justifyContent: "center",
+      borderRadius: 30,
+      height: 60,
+      alignItems: "center",
     },
+    cancelButtonReceive: {
+      borderWidth: 3,
+      borderColor: buttonBackgroundColor,
+      padding: 10,
+      width: "100%",
+      justifyContent: "center",
+      borderRadius: 30,
+      height: 60,
+      alignItems: "center",
+      marginTop: 20,
+    },
+    modalView: {
+      margin: 20,
+      height: 500,
+      width: "90%",
+      backgroundColor: modalBackgroundColor,
+      borderRadius: 20,
+      padding: 35,
+      alignItems: "center",
+      justifyContent: "space-between",
+    },
+    inputModelView: {
+      margin: 20,
+      height: 400,
+      width: "90%",
+      backgroundColor: modalBackgroundColor,
+      borderRadius: 20,
+      padding: 35,
+      alignItems: "center",
+    },
+
+    confirmModalView: {
+      margin: 20,
+      alignSelf: "center",
+      maxHeight: "90%",
+      width: "90%",
+      backgroundColor: modalBackgroundColor,
+      borderRadius: 20,
+      padding: 35,
+      alignItems: "center",
+      justifyContent: "space-between",
+    },
+
     addressText: {
       color: secondTextColor,
       textAlign: "center",
@@ -255,27 +264,58 @@ const TransactionsScreenStyles = (isDarkMode) => {
       flexWrap: "wrap",
       width: 280,
     },
-    disconnectButtonText: {
-      color: "#FFFFFF",
+    cancelButtonText: {
+      color: secondTextColor,
+      fontSize: 16,
+    },
+    transactionText: {
+      color: secondTextColor,
+      fontSize: 16,
+      marginBottom: 10,
+    },
+    modalTitle: {
+      color: titleColor,
+      fontSize: 16,
       fontWeight: "bold",
     },
-    TransactionModalTitle: {
-      fontSize: 16,
+    modalReceiveTitle: {
       color: titleColor,
-      textAlign: "center",
-      marginBottom: 20,
-      lineHeight: 30,
+      fontSize: 16,
+      fontWeight: "bold",
+    },
+    receiveModalView: {
+      margin: 20,
+      height: 600,
+      width: "90%",
+      backgroundColor: modalBackgroundColor,
+      borderRadius: 20,
+      padding: 35,
+      justifyContent: "space-between",
+      alignItems: "center",
+    },
+    centeredView: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "rgba(0, 0, 0, 0.5)",
+    },
+    optionButtonText: {
+      color: textBtnColor,
+    },
+    optionButton: {
+      backgroundColor: btnColor,
+      padding: 10,
+      width: "100%",
+      justifyContent: "center",
+      borderRadius: 30,
+      height: 60,
+      alignItems: "center",
+      marginBottom: 16,
     },
 
-    // 输入框样式
-    passwordInput: {
-      backgroundColor: inputBackgroundColor,
-      paddingHorizontal: 15,
-      paddingVertical: 10,
-      borderRadius: 10,
-      height: 50,
-      width: "100%",
-      color: textColor,
+    submitButtonText: {
+      color: textBtnColor,
+      fontSize: 16,
     },
     input: {
       backgroundColor: inputBackgroundColor,
@@ -290,13 +330,23 @@ const TransactionsScreenStyles = (isDarkMode) => {
       backgroundColor: inputBackgroundColor,
       padding: 10,
       marginTop: 20,
+      // marginBottom: 20,
       justifyContent: "center",
       borderRadius: 10,
       height: 60,
       alignItems: "center",
     },
+    amountModalView: {
+      margin: 20,
+      width: "90%",
+      backgroundColor: modalBackgroundColor,
+      borderRadius: 20,
+      padding: 35,
+      alignItems: "center",
+      flex: 0, // 默认值：内部内容会撑开容器高度
+    },
 
-    // 历史记录样式
+    /*  History */
     historyItemText: {
       fontSize: 16,
       color: textColor,
@@ -329,17 +379,7 @@ const TransactionsScreenStyles = (isDarkMode) => {
       width: 360,
       height: 360,
     },
-
-    // 其他样式
-    bgContainer: {
-      flex: 1,
-      backgroundColor: backgroundColor,
-      alignItems: "center",
-      padding: 20,
-    },
-    container: {
-      alignItems: "center",
-    },
+    /*  blurView */
     blurView: {
       position: "absolute",
       top: 0,
@@ -357,26 +397,48 @@ const TransactionsScreenStyles = (isDarkMode) => {
       fontSize: 12,
       textAlign: "center",
     },
+    buttonText: {
+      color: textBtnColor,
+      fontSize: 16,
+      fontWeight: "bold",
+      marginBottom: 2,
+    },
     mainButtonText: {
       color: titleColor,
       fontSize: 16,
       fontWeight: "bold",
       marginVertical: 10,
     },
-    centeredView: {
-      flex: 1,
-      justifyContent: "center",
+    roundButton: {
+      borderWidth: 2,
+      borderColor: btnBorderColor,
+      paddingVertical: 10,
+      paddingHorizontal: 20,
+      width: 170,
+      height: 170,
+      borderRadius: 20,
       alignItems: "center",
-      backgroundColor: "rgba(0, 0, 0, 0.5)",
+      justifyContent: "center",
+      marginBottom: 20,
     },
-    bluetoothImg: {
-      width: 150,
-      height: 150,
-      marginBottom: 30,
+
+    bgContainer: {
+      flex: 1,
+      backgroundColor: backgroundColor,
+      alignItems: "center",
+      // justifyContent: "center",
+      padding: 20,
     },
-    modalText: {
-      color: secondTextColor,
+    container: {
+      alignItems: "center",
+    },
+    modalText: { color: secondTextColor, textAlign: "center" },
+    TransactionModalTitle: {
+      fontSize: 16,
+      color: titleColor,
       textAlign: "center",
+      marginBottom: 20,
+      lineHeight: 30,
     },
   });
 };
