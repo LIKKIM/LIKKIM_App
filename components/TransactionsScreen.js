9afb9c4faaab22a676cb7525619a81fc3607344e
Jaime Lee <18038163520@163.com>
2024-09-09 17:31:33 +0800

    1

--- components/TransactionsScreen.js
+++ components/TransactionsScreen.js
@@ -29,7 +29,7 @@ import base64 from "base64-js";
 import { Buffer } from "buffer";
 import VerificationModal from "./modal/VerificationModal";
 import BluetoothModal from "./modal/BluetoothModal";
-
+import AmountModal from "./modal/AmountModal";
 import { BleManager, BleErrorCode } from "react-native-ble-plx";
 const serviceUUID = "0000FFE0-0000-1000-8000-00805F9B34FB";
 const writeCharacteristicUUID = "0000FFE2-0000-1000-8000-00805F9B34FB";
@@ -1580,161 +1580,25 @@ function TransactionsScreen() {
         />
 
         {/* 输入金额的 Modal */}
-        <Modal
-          animationType="slide"
-          transparent={true}
-          visible={amountModalVisible}
-          onRequestClose={() => setAmountModalVisible(false)}
-        >
-          <KeyboardAvoidingView
-            behavior={Platform.OS === "ios" ? "padding" : "height"}
-            style={TransactionsScreenStyle.centeredView}
-          >
-            <BlurView
-              intensity={10}
-              style={TransactionsScreenStyle.blurBackground}
-            />
-            <View style={TransactionsScreenStyle.amountModalView}>
-              <View
-                style={{
-                  flexDirection: "row",
-                  alignItems: "center",
-                  marginBottom: 10,
-                }}
-              >
-                {selectedCryptoIcon && (
-                  <Image
-                    source={selectedCryptoIcon}
-                    style={{ width: 24, height: 24, marginRight: 8 }}
-                  />
-                )}
-                <Text style={TransactionsScreenStyle.modalTitle}>
-                  {selectedCrypto} ({selectedCryptoChain})
-                </Text>
-              </View>
-
-              {/* 显示余额和相关信息 */}
-              <View style={{ marginBottom: 15 }}>
-                {/*       <Text style={TransactionsScreenStyle.infoText}>
-                  {t("Price per unit in USD")}: ${priceUsd}
-                </Text> */}
-              </View>
-
-              <Text style={TransactionsScreenStyle.modalTitle}>
-                {t("Enter the amount to send:")}
-              </Text>
-              <View
-                style={{
-                  width: "100%",
-
-                  alignItems: "center",
-                }}
-              >
-                <TextInput
-                  style={[
-                    TransactionsScreenStyle.amountInput,
-                    {
-                      color: isDarkMode ? "#ffffff" : "#000000", // 字体颜色
-                      backgroundColor: "transparent", // 去掉背景颜色
-                      fontSize: 30, // 设置大字号
-                      textAlign: "center", // 输入的文本居中
-                      fontWeight: "bold", // 设置粗体字
-                    },
-                  ]}
-                  placeholder={t("Enter Amount")}
-                  placeholderTextColor={isDarkMode ? "#808080" : "#cccccc"}
-                  keyboardType="numeric"
-                  onChangeText={(text) => {
-                    // Normalize the input by removing leading zeros
-                    let normalizedText = text.replace(/^0+(?!\.|$)/, "");
-
-                    // Allow only valid decimal numbers with up to 10 decimal places
-                    const regex = /^\d*\.?\d{0,10}$/;
-                    if (regex.test(normalizedText)) {
-                      setAmount(normalizedText);
-                    }
-                  }}
-                  value={amount}
-                  autoFocus={true}
-                  caretHidden={true} // 隐藏光标
-                />
-                <Text style={TransactionsScreenStyle.balanceModalSubtitle}>
-                  {t("Balance")}: {balance} {selectedCrypto}
-                </Text>
-              </View>
-              <View
-                style={{
-                  flexDirection: "row",
-                  width: 280,
-                  justifyContent: "space-between",
-                  marginBottom: 20,
-                }}
-              >
-                <View
-                  style={{
-                    justifyContent: "flex-start", // 使内容从上到下排列
-                  }}
-                >
-                  <Text style={TransactionsScreenStyle.balanceSubtitle}>
-                    {t("USD Value")}: ${valueUsd}
-                  </Text>
-                  {/* 当用户输入的金额大于余额时显示余额不足 */}
-                  {parseFloat(amount) >
-                    parseFloat(balance) + parseFloat(fee) && (
-                    <Text
-                      style={[
-                        TransactionsScreenStyle.balanceSubtitle,
-                        { color: "#FF5252", marginTop: 5 }, // 使用 marginTop 而不是 marginLeft
-                      ]}
-                    >
-                      {t("Insufficient Balance")}
-                    </Text>
-                  )}
-                </View>
-
-                <Text style={TransactionsScreenStyle.balanceSubtitle}>
-                  {t("Fee")}: {fee} {selectedCrypto}
-                </Text>
-              </View>
-
-              <View
-                style={{
-                  flexDirection: "column",
-                  justifyContent: "space-between",
-                  width: "100%",
-                }}
-              >
-                <TouchableOpacity
-                  style={[
-                    TransactionsScreenStyle.optionButton,
-                    {
-                      backgroundColor: isAmountValid
-                        ? buttonBackgroundColor
-                        : disabledButtonBackgroundColor,
-                    },
-                  ]}
-                  onPress={handleNextAfterAmount}
-                  disabled={!isAmountValid}
-                >
-                  <Text style={TransactionsScreenStyle.submitButtonText}>
-                    {t("Next")}
-                  </Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={TransactionsScreenStyle.cancelButton}
-                  onPress={() => {
-                    setAmountModalVisible(false);
-                    setInputAddressModalVisible(true);
-                  }}
-                >
-                  <Text style={TransactionsScreenStyle.cancelButtonText}>
-                    {t("Back")}
-                  </Text>
-                </TouchableOpacity>
-              </View>
-            </View>
-          </KeyboardAvoidingView>
-        </Modal>
+        <AmountModal
+          visible={amountModalVisible} // 控制模态框是否可见
+          onRequestClose={() => setAmountModalVisible(false)} // 关闭模态框的回调函数
+          TransactionsScreenStyle={TransactionsScreenStyle} // 样式对象
+          t={t} // 翻译函数，用于国际化
+          isDarkMode={isDarkMode} // 暗黑模式状态
+          amount={amount} // 当前输入的金额
+          setAmount={setAmount} // 更新金额的回调函数
+          balance={balance} // 用户的加密货币余额
+          fee={fee} // 交易费用
+          valueUsd={valueUsd} // 输入金额对应的美元价值
+          isAmountValid={isAmountValid} // 检查输入金额是否有效的状态
+          buttonBackgroundColor={buttonBackgroundColor} // 按钮背景色
+          disabledButtonBackgroundColor={disabledButtonBackgroundColor} // 禁用按钮时的背景色
+          handleNextAfterAmount={handleNextAfterAmount} // 处理用户点击"Next"后的逻辑
+          selectedCrypto={selectedCrypto} // 当前选择的加密货币
+          selectedCryptoIcon={selectedCryptoIcon} // 当前加密货币的图标
+          selectedCryptoChain={selectedCryptoChain} // 传递当前加密货币的链名称
+        />
 
         {/* 交易确认的 Modal */}
         <Modal
---
+++ components/modal/AmountModal.js
@@ -0,0 +1,184 @@
+// AmountModal.js
+import React from "react";
+import {
+  Modal,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import { BlurView } from "expo-blur";
+
+const AmountModal = ({
+  visible,
+  onRequestClose,
+  TransactionsScreenStyle,
+  t,
+  isDarkMode,
+  amount,
+  setAmount,
+  balance,
+  fee,
+  valueUsd,
+  isAmountValid,
+  buttonBackgroundColor,
+  disabledButtonBackgroundColor,
+  handleNextAfterAmount,
+  selectedCrypto,
+  selectedCryptoChain,
+  selectedCryptoIcon,
+}) => {
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={visible}
+      onRequestClose={onRequestClose}
+    >
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={TransactionsScreenStyle.centeredView}
+      >
+        <BlurView
+          intensity={10}
+          style={TransactionsScreenStyle.blurBackground}
+        />
+        <View style={TransactionsScreenStyle.amountModalView}>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginBottom: 10,
+            }}
+          >
+            {selectedCryptoIcon && (
+              <Image
+                source={selectedCryptoIcon}
+                style={{ width: 24, height: 24, marginRight: 8 }}
+              />
+            )}
+            <Text style={TransactionsScreenStyle.modalTitle}>
+              {selectedCrypto} ({selectedCryptoChain})
+            </Text>
+          </View>
+
+          {/* 显示余额和相关信息 */}
+          <View style={{ marginBottom: 15 }}>
+            {/*  <Text style={TransactionsScreenStyle.infoText}>
+              {t("Price per unit in USD")}: ${priceUsd}
+            </Text> */}
+          </View>
+
+          <Text style={TransactionsScreenStyle.modalTitle}>
+            {t("Enter the amount to send:")}
+          </Text>
+
+          <View style={{ width: "100%", alignItems: "center" }}>
+            <TextInput
+              style={[
+                TransactionsScreenStyle.amountInput,
+                {
+                  color: isDarkMode ? "#ffffff" : "#000000",
+                  backgroundColor: "transparent", // 去掉背景颜色
+                  fontSize: 30, // 设置大字号
+                  textAlign: "center", // 输入的文本居中
+                  fontWeight: "bold", // 设置粗体字
+                },
+              ]}
+              placeholder={t("Enter Amount")}
+              placeholderTextColor={isDarkMode ? "#808080" : "#cccccc"}
+              keyboardType="numeric"
+              onChangeText={(text) => {
+                // Normalize the input by removing leading zeros
+                let normalizedText = text.replace(/^0+(?!\.|$)/, "");
+
+                // Allow only valid decimal numbers with up to 10 decimal places
+                const regex = /^\d*\.?\d{0,10}$/;
+                if (regex.test(normalizedText)) {
+                  setAmount(normalizedText);
+                }
+              }}
+              value={amount}
+              autoFocus={true}
+              caretHidden={true} // 隐藏光标
+            />
+            <Text style={TransactionsScreenStyle.balanceModalSubtitle}>
+              {t("Balance")}: {balance} {selectedCrypto}
+            </Text>
+          </View>
+
+          <View
+            style={{
+              flexDirection: "row",
+              width: 280,
+              justifyContent: "space-between",
+              marginBottom: 20,
+            }}
+          >
+            <View style={{ justifyContent: "flex-start" }}>
+              <Text style={TransactionsScreenStyle.balanceSubtitle}>
+                {t("USD Value")}: ${valueUsd}
+              </Text>
+
+              {/* 当用户输入的金额大于余额时显示余额不足 */}
+              {parseFloat(amount) > parseFloat(balance) + parseFloat(fee) && (
+                <Text
+                  style={[
+                    TransactionsScreenStyle.balanceSubtitle,
+                    { color: "#FF5252", marginTop: 5 },
+                  ]}
+                >
+                  {t("Insufficient Balance")}
+                </Text>
+              )}
+            </View>
+
+            <Text style={TransactionsScreenStyle.balanceSubtitle}>
+              {t("Fee")}: {fee} {selectedCrypto}
+            </Text>
+          </View>
+
+          <View
+            style={{
+              flexDirection: "column",
+              justifyContent: "space-between",
+              width: "100%",
+            }}
+          >
+            <TouchableOpacity
+              style={[
+                TransactionsScreenStyle.optionButton,
+                {
+                  backgroundColor: isAmountValid
+                    ? buttonBackgroundColor
+                    : disabledButtonBackgroundColor,
+                },
+              ]}
+              onPress={handleNextAfterAmount}
+              disabled={!isAmountValid}
+            >
+              <Text style={TransactionsScreenStyle.submitButtonText}>
+                {t("Next")}
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={TransactionsScreenStyle.cancelButton}
+              onPress={() => {
+                onRequestClose();
+              }}
+            >
+              <Text style={TransactionsScreenStyle.cancelButtonText}>
+                {t("Back")}
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+    </Modal>
+  );
+};
+
+export default AmountModal;
