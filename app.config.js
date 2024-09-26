fb08b70e3c343113b4adb9591c7b15015772194e
Jaime Lee <18038163520@163.com>
2024-09-26 12:38:12 +0800

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -27,7 +27,7 @@ export default {
       UIBackgroundModes: ["bluetooth-central", "bluetooth-peripheral"],
     },
     supportsTablet: true,
-    buildNumber: "15.8.7",
+    buildNumber: "15.8.8",
     bundleIdentifier: "RN.LKKIM",
     icon: "./assets/icon.png",
   },
