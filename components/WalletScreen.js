69b9692d2525c90f5fdf92d50cbee9b2b052be6f
Jaime Lee <18038163520@163.com>
2024-12-07 02:49:45 +0800

    Update TransactionsScreen.js

--- components/WalletScreen.js
+++ components/WalletScreen.js
@@ -941,8 +941,6 @@ function WalletScreen({ route, navigation }) {
 
   // 监听钱包生成结果
   const monitorWalletCreationResult = (device) => {
-    const notifyCharacteristicUUID = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E";
-
     monitorSubscription = device.monitorCharacteristicForService(
       serviceUUID,
       notifyCharacteristicUUID,
