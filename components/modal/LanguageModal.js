3f4b522910dcd3c565e9b943690042fb48d6d7d6
Jaime Lee <18038163520@163.com>
2024-12-11 23:36:31 +0800

    Update LanguageModal.js

--- components/modal/LanguageModal.js
+++ components/modal/LanguageModal.js
@@ -9,6 +9,8 @@ import {
   Modal,
   TouchableWithoutFeedback,
   Keyboard,
+  KeyboardAvoidingView,
+  Platform,
 } from "react-native";
 import { BlurView } from "expo-blur";
 import Icon from "react-native-vector-icons/MaterialIcons";
@@ -38,56 +40,61 @@ const LanguageModal = ({
       {/* 点击模糊区域关闭模态框 */}
       <TouchableWithoutFeedback
         onPress={() => {
-          Keyboard.dismiss(); // 关闭键盘（可选）
+          Keyboard.dismiss(); // 关闭键盘
           onClose(); // 关闭 Modal
         }}
       >
         <View style={{ flex: 1 }}>
-          <BlurView intensity={10} style={styles.centeredView}>
-            <View
-              style={styles.languageModalView}
-              // 阻止点击事件冒泡，避免触发关闭 Modal
-              onStartShouldSetResponder={() => true}
-            >
-              <Text style={styles.languageModalTitle}>
-                {t("Select Language")}
-              </Text>
-
-              <View style={styles.searchContainer}>
-                <Icon name="search" size={20} style={styles.searchIcon} />
-                <TextInput
-                  style={styles.searchInput}
-                  placeholder={t("Search Language")}
-                  placeholderTextColor={isDarkMode ? "#ccc" : "#666"}
-                  onChangeText={setSearchLanguage}
-                  value={searchLanguage}
-                />
-              </View>
-
-              <ScrollView style={styles.languageList}>
-                {filteredLanguages.map((language) => (
-                  <TouchableOpacity
-                    style={{
-                      marginBottom: 6,
-                    }}
-                    key={language.code}
-                    onPress={() => handleLanguageChange(language)}
-                  >
-                    <Text style={styles.languageModalText}>
-                      {language.name}
-                    </Text>
-                  </TouchableOpacity>
-                ))}
-              </ScrollView>
-
-              <TouchableOpacity
-                style={styles.languageCancelButton}
-                onPress={onClose}
+          <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={{ flex: 1 }}
+          >
+            <BlurView intensity={10} style={styles.centeredView}>
+              <View
+                style={styles.languageModalView}
+                // 阻止点击事件冒泡，避免触发关闭 Modal
+                onStartShouldSetResponder={() => true}
               >
-                <Text style={styles.cancelButtonText}>{t("Cancel")}</Text>
-              </TouchableOpacity>
-            </View>
-          </BlurView>
+                <Text style={styles.languageModalTitle}>
+                  {t("Select Language")}
+                </Text>
+
+                <View style={styles.searchContainer}>
+                  <Icon name="search" size={20} style={styles.searchIcon} />
+                  <TextInput
+                    style={styles.searchInput}
+                    placeholder={t("Search Language")}
+                    placeholderTextColor={isDarkMode ? "#ccc" : "#666"}
+                    onChangeText={setSearchLanguage}
+                    value={searchLanguage}
+                  />
+                </View>
+
+                <ScrollView style={styles.languageList}>
+                  {filteredLanguages.map((language) => (
+                    <TouchableOpacity
+                      style={{
+                        marginBottom: 6,
+                      }}
+                      key={language.code}
+                      onPress={() => handleLanguageChange(language)}
+                    >
+                      <Text style={styles.languageModalText}>
+                        {language.name}
+                      </Text>
+                    </TouchableOpacity>
+                  ))}
+                </ScrollView>
+
+                <TouchableOpacity
+                  style={styles.languageCancelButton}
+                  onPress={onClose}
+                >
+                  <Text style={styles.cancelButtonText}>{t("Cancel")}</Text>
+                </TouchableOpacity>
+              </View>
+            </BlurView>
+          </KeyboardAvoidingView>
         </View>
       </TouchableWithoutFeedback>
     </Modal>
