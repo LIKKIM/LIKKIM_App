c6f5a262a82f2011fd8f90ff9fab6c39133451f9
Jaime Lee <18038163520@163.com>
2024-12-11 23:40:20 +0800

    Update CurrencyModal.js

--- components/modal/CurrencyModal.js
+++ components/modal/CurrencyModal.js
@@ -7,6 +7,10 @@ import {
   TextInput,
   ScrollView,
   Modal,
+  TouchableWithoutFeedback,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
 } from "react-native";
 import { BlurView } from "expo-blur";
 import Icon from "react-native-vector-icons/MaterialIcons";
@@ -35,44 +39,66 @@ const CurrencyModal = ({
       visible={visible}
       onRequestClose={onClose}
     >
-      <BlurView intensity={10} style={styles.centeredView}>
-        <View style={styles.currencyModalView}>
-          <Text style={styles.languageModalTitle}>{t("Select Currency")}</Text>
-
-          {/* Search Box */}
-          <View style={styles.searchContainer}>
-            <Icon name="search" size={20} style={styles.searchIcon} />
-            <TextInput
-              style={styles.searchInput}
-              placeholder={t("Search Currency")}
-              placeholderTextColor={isDarkMode ? "#ccc" : "#666"}
-              onChangeText={setSearchCurrency}
-              value={searchCurrency}
-            />
-          </View>
-
-          <ScrollView style={styles.languageList}>
-            {filteredCurrencies.map((currency) => (
-              <TouchableOpacity
-                key={currency.shortName}
-                style={styles.currencyOption}
-                onPress={() => handleCurrencyChange(currency)}
-              >
-                <Text style={styles.languageModalText}>
-                  {currency.name} - {currency.shortName}
-                </Text>
-              </TouchableOpacity>
-            ))}
-          </ScrollView>
-
-          <TouchableOpacity
-            style={styles.languageCancelButton}
-            onPress={onClose}
+      {/* 点击模糊区域关闭键盘和Modal */}
+      <TouchableWithoutFeedback
+        onPress={() => {
+          Keyboard.dismiss(); // 关闭键盘
+          onClose(); // 关闭 Modal
+        }}
+      >
+        <View style={{ flex: 1 }}>
+          <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={{ flex: 1 }}
           >
-            <Text style={styles.cancelButtonText}>{t("Cancel")}</Text>
-          </TouchableOpacity>
+            <BlurView intensity={10} style={styles.centeredView}>
+              <View
+                style={styles.currencyModalView}
+                onStartShouldSetResponder={() => true} // 阻止点击事件冒泡
+              >
+                <Text style={styles.languageModalTitle}>
+                  {t("Select Currency")}
+                </Text>
+
+                {/* Search Box */}
+                <View style={styles.searchContainer}>
+                  <Icon name="search" size={20} style={styles.searchIcon} />
+                  <TextInput
+                    style={styles.searchInput}
+                    placeholder={t("Search Currency")}
+                    placeholderTextColor={isDarkMode ? "#ccc" : "#666"}
+                    onChangeText={setSearchCurrency}
+                    value={searchCurrency}
+                  />
+                </View>
+
+                <ScrollView style={styles.languageList}>
+                  {filteredCurrencies.map((currency) => (
+                    <TouchableOpacity
+                      key={currency.shortName}
+                      style={{
+                        marginBottom: 6,
+                      }}
+                      onPress={() => handleCurrencyChange(currency)}
+                    >
+                      <Text style={styles.languageModalText}>
+                        {currency.name} - {currency.shortName}
+                      </Text>
+                    </TouchableOpacity>
+                  ))}
+                </ScrollView>
+
+                <TouchableOpacity
+                  style={styles.languageCancelButton}
+                  onPress={onClose}
+                >
+                  <Text style={styles.cancelButtonText}>{t("Cancel")}</Text>
+                </TouchableOpacity>
+              </View>
+            </BlurView>
+          </KeyboardAvoidingView>
         </View>
-      </BlurView>
+      </TouchableWithoutFeedback>
     </Modal>
   );
 };
