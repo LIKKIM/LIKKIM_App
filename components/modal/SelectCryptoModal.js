455d1a6832eb8d16836341b0ccbac7360c05a878
Jaime Lee <18038163520@163.com>
2024-12-11 23:13:15 +0800

    Update SelectCryptoModal.js

--- components/modal/SelectCryptoModal.js
+++ components/modal/SelectCryptoModal.js
@@ -9,9 +9,11 @@ import {
   TextInput,
   KeyboardAvoidingView,
   Platform,
+  TouchableWithoutFeedback, // 引入TouchableWithoutFeedback
 } from "react-native";
 import { BlurView } from "expo-blur";
 import Icon from "react-native-vector-icons/MaterialIcons";
+
 const SelectCryptoModal = ({
   visible,
   onRequestClose,
@@ -39,100 +41,107 @@ const SelectCryptoModal = ({
       visible={visible}
       onRequestClose={onRequestClose}
     >
-      <KeyboardAvoidingView
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-        style={{ flex: 1 }}
-      >
-        <BlurView intensity={10} style={TransactionsScreenStyle.centeredView}>
-          <View style={TransactionsScreenStyle.modalView}>
-            <Text style={TransactionsScreenStyle.TransactionModalTitle}>
-              {addedCryptos.length === 0
-                ? t("No cryptocurrencies available. Please add wallet first.")
-                : operationType === "send"
-                ? t("Choose the cryptocurrency to send:")
-                : t("Choose the cryptocurrency to receive:")}
-            </Text>
-            {/* 添加搜索框 */}
-            {addedCryptos.length > 0 && (
-              <View style={TransactionsScreenStyle.searchContainer}>
-                <Icon
-                  name="search"
-                  size={20}
-                  style={TransactionsScreenStyle.searchIcon}
-                />
-                <TextInput
-                  style={TransactionsScreenStyle.searchInput}
-                  placeholder={t("Search Cryptocurrency")}
-                  placeholderTextColor={isDarkMode ? "#ffffff" : "#21201E"}
-                  onChangeText={(text) => setSearchQuery(text)}
-                  value={searchQuery}
-                />
-              </View>
-            )}
-
-            {/* 如果没有加密货币 */}
-            {addedCryptos.length === 0 ? (
-              <View
-                style={{
-                  justifyContent: "center",
-                  alignItems: "center",
-                  height: 260,
-                }}
-              >
-                <Image
-                  source={require("../../assets/gif/Empty.gif")}
-                  style={{
-                    width: 200,
-                    height: 200,
-                    marginBottom: 40,
-                  }}
-                />
-              </View>
-            ) : (
-              <ScrollView
-                contentContainerStyle={{ alignItems: "center" }}
-                style={{ maxHeight: 400, width: 320, paddingHorizontal: 20 }}
-              >
-                {filteredCryptos.map((crypto) => (
-                  <TouchableOpacity
-                    key={`${crypto.shortName}_${crypto.chain}`}
-                    style={TransactionsScreenStyle.optionButton}
-                    onPress={() => selectCrypto(crypto)}
-                  >
-                    <View
-                      style={{ flexDirection: "row", alignItems: "center" }}
-                    >
-                      {crypto.icon && (
-                        <Image
-                          source={crypto.icon}
-                          style={{
-                            width: 24,
-                            height: 24,
-                            marginRight: 8,
-                            backgroundColor: "rgba(255, 255, 255, 0.2)",
-                            borderRadius: 12,
-                          }}
-                        />
-                      )}
-                      <Text style={TransactionsScreenStyle.optionButtonText}>
-                        {crypto.shortName} ({crypto.chain})
-                      </Text>
-                    </View>
-                  </TouchableOpacity>
-                ))}
-              </ScrollView>
-            )}
-            <TouchableOpacity
-              style={TransactionsScreenStyle.cancelButtonReceive}
-              onPress={() => setModalVisible(false)}
+      {/* 监听模糊背景点击 */}
+      <TouchableWithoutFeedback onPress={() => setModalVisible(false)}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          style={{ flex: 1 }}
+        >
+          <BlurView intensity={10} style={TransactionsScreenStyle.centeredView}>
+            <View
+              style={TransactionsScreenStyle.modalView}
+              // 阻止点击事件冒泡，避免触发关闭 Modal
+              onStartShouldSetResponder={() => true}
             >
-              <Text style={TransactionsScreenStyle.cancelButtonText}>
-                {t("Cancel")}
+              <Text style={TransactionsScreenStyle.TransactionModalTitle}>
+                {addedCryptos.length === 0
+                  ? t("No cryptocurrencies available. Please add wallet first.")
+                  : operationType === "send"
+                  ? t("Choose the cryptocurrency to send:")
+                  : t("Choose the cryptocurrency to receive:")}
               </Text>
-            </TouchableOpacity>
-          </View>
-        </BlurView>
-      </KeyboardAvoidingView>
+              {/* 添加搜索框 */}
+              {addedCryptos.length > 0 && (
+                <View style={TransactionsScreenStyle.searchContainer}>
+                  <Icon
+                    name="search"
+                    size={20}
+                    style={TransactionsScreenStyle.searchIcon}
+                  />
+                  <TextInput
+                    style={TransactionsScreenStyle.searchInput}
+                    placeholder={t("Search Cryptocurrency")}
+                    placeholderTextColor={isDarkMode ? "#ffffff" : "#21201E"}
+                    onChangeText={(text) => setSearchQuery(text)}
+                    value={searchQuery}
+                  />
+                </View>
+              )}
+
+              {/* 如果没有加密货币 */}
+              {addedCryptos.length === 0 ? (
+                <View
+                  style={{
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: 260,
+                  }}
+                >
+                  <Image
+                    source={require("../../assets/gif/Empty.gif")}
+                    style={{
+                      width: 200,
+                      height: 200,
+                      marginBottom: 40,
+                    }}
+                  />
+                </View>
+              ) : (
+                <ScrollView
+                  contentContainerStyle={{ alignItems: "center" }}
+                  style={{ maxHeight: 400, width: 320, paddingHorizontal: 20 }}
+                >
+                  {filteredCryptos.map((crypto) => (
+                    <TouchableOpacity
+                      key={`${crypto.shortName}_${crypto.chain}`}
+                      style={TransactionsScreenStyle.optionButton}
+                      onPress={() => selectCrypto(crypto)}
+                    >
+                      <View
+                        style={{ flexDirection: "row", alignItems: "center" }}
+                      >
+                        {crypto.icon && (
+                          <Image
+                            source={crypto.icon}
+                            style={{
+                              width: 24,
+                              height: 24,
+                              marginRight: 8,
+                              backgroundColor: "rgba(255, 255, 255, 0.2)",
+                              borderRadius: 12,
+                            }}
+                          />
+                        )}
+                        <Text style={TransactionsScreenStyle.optionButtonText}>
+                          {crypto.shortName} ({crypto.chain})
+                        </Text>
+                      </View>
+                    </TouchableOpacity>
+                  ))}
+                </ScrollView>
+              )}
+              <TouchableOpacity
+                style={TransactionsScreenStyle.cancelButtonReceive}
+                onPress={() => setModalVisible(false)}
+              >
+                <Text style={TransactionsScreenStyle.cancelButtonText}>
+                  {t("Cancel")}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </BlurView>
+        </KeyboardAvoidingView>
+      </TouchableWithoutFeedback>
     </Modal>
   );
 };
