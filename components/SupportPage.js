477fc5e3fd1de4fdc9072986ce6da6e160646389
Jaime Lee <18038163520@163.com>
2024-12-30 17:23:30 +0800

    Update SupportPage.js

--- components/SupportPage.js
+++ components/SupportPage.js
@@ -1,38 +1,43 @@
 // src/components/SupportPage.js
-import React, {
-  useState,
-  useEffect,
-  useContext,
-  useRef,
-  useLayoutEffect,
-} from "react";
-import { View, Text } from "react-native";
-import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import React, { useContext, useLayoutEffect } from "react";
+import { View, Text, StyleSheet } from "react-native";
 import { useTranslation } from "react-i18next";
+import { useNavigation } from "@react-navigation/native";
+import { LinearGradient } from "expo-linear-gradient";
 import { DarkModeContext } from "./CryptoContext";
+
 const SupportPage = () => {
   const { isDarkMode } = useContext(DarkModeContext);
-  const { t } = useTranslation();
   const navigation = useNavigation();
+  const { t } = useTranslation();
+
+  const darkColors = ["#21201E", "#0E0D0D"];
+  const lightColors = ["#FFFFFF", "#EDEBEF"];
+
   useLayoutEffect(() => {
     navigation.setOptions({
       headerStyle: {
         backgroundColor: isDarkMode ? "#21201E" : "#FFFFFF",
       },
       headerTintColor: isDarkMode ? "#FFFFFF" : "#000000",
+      headerBackTitle: t("Back"),
     });
-  }, [isDarkMode, navigation]);
-  useEffect(() => {
-    navigation.setOptions({
-      headerBackTitle: t("Back"), // 设置国际化的“返回”按钮文本
-    });
-  }, [navigation, t]);
+  }, [isDarkMode, navigation, t]);
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Support Information</Text>
-    </View>
+    <LinearGradient
+      style={styles.container}
+      colors={isDarkMode ? darkColors : lightColors}
+    ></LinearGradient>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default SupportPage;
