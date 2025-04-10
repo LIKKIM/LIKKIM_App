714722973fb561e95bf214f14331c57d91aab0e5
Jaime Lee <18038163520@163.com>
2025-04-10 14:22:10 +0800

    Update TabModal.js

--- components/walletScreen/TabModal.js
+++ components/walletScreen/TabModal.js
@@ -10,7 +10,7 @@ import {
   Modal,
   Dimensions,
 } from "react-native";
-import PriceChartCom from "../walletScreen/PriceChartCom";
+import PriceChartCom from "../WalletScreen/PriceChartCom";
 import { LinearGradient } from "expo-linear-gradient";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
