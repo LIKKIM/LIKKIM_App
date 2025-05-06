cb60ef7dc73aa08ef0d03677fb9ab441f1373649
Jaime Lee <18038163520@163.com>
2025-05-06 15:03:31 +0800

    update TransactionsScreen.js

--- env/apiEndpoints.js
+++ env/apiEndpoints.js
@@ -4,3 +4,12 @@ export const swapAPI = {
   queryQuote: "https://swap.likkim.com/api/aggregator/queryQuote",
   executeSwap: "https://swap.likkim.com/api/aggregator/swap",
 };
+
+export const walletAPI = {
+  queryTransaction: "https://bt.likkim.com/api/wallet/queryTransaction",
+  blockchainFee: "https://bt.likkim.com/api/chain/blockchain-fee",
+  balance: "https://bt.likkim.com/api/wallet/balance",
+  broadcastHex: "https://bt.likkim.com/api/wallet/broadcastHex",
+  getSignParam: "https://bt.likkim.com/api/wallet/getSignParam",
+  encodeEvm: "https://bt.likkim.com/api/sign/encode_evm",
+};
