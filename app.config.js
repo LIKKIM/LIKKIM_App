31762740a54d3022fa2391b2fdf4bd7b541a94cc
Jaime Lee <18038163520@163.com>
2025-06-01 18:58:36 +0800

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -40,7 +40,7 @@ export default {
         "Allow $(PRODUCT_NAME) to communicate with your device via NFC.",
     },
     supportsTablet: false,
-    buildNumber: "16.3.9",
+    buildNumber: "16.3.10",
     bundleIdentifier: "com.likkim.likkim", //❌
     icon: "./assets/icon.png", //❌
   },
