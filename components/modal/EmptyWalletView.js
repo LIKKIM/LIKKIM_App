97e90f9892a46be625e23848c2c13a345539f149
Jaime Lee <18038163520@163.com>
2025-07-29 02:47:25 +0800

    Update EmptyWalletView.js

--- components/modal/EmptyWalletView.js
+++ components/modal/EmptyWalletView.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { View, ImageBackground, TouchableOpacity, Text } from "react-native";
+import {
+  View,
+  ImageBackground,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+} from "react-native";
+import { Video } from "expo-av";
 
 const EmptyWalletView = ({
   isDarkMode,
@@ -10,25 +17,43 @@ const EmptyWalletView = ({
 }) => {
   return (
     <View style={VaultScreenStyle.centeredContent}>
-      <ImageBackground
-        source={
-          isDarkMode
-            ? require("../../assets/AddWallet.png")
-            : require("../../assets/Card22.png")
-        }
-        style={VaultScreenStyle.addWalletImage}
-        imageStyle={VaultScreenStyle.addWalletImageBorder}
-      >
-        <TouchableOpacity
-          //   onPress={handleContinue}
-          onPress={handleWalletTest} //      {/*  测试按钮test btn  */}
-          style={VaultScreenStyle.addWalletButton}
+      {isDarkMode ? (
+        <ImageBackground
+          source={require("../../assets/AddWallet.png")}
+          style={VaultScreenStyle.addWalletImage}
+          imageStyle={VaultScreenStyle.addWalletImageBorder}
         >
-          <Text style={VaultScreenStyle.addWalletButtonText}>
-            {t("Get Started")}
-          </Text>
-        </TouchableOpacity>
-      </ImageBackground>
+          <TouchableOpacity
+            //   onPress={handleContinue}
+            onPress={handleWalletTest} //      {/*  测试按钮test btn  */}
+            style={VaultScreenStyle.addWalletButton}
+          >
+            <Text style={VaultScreenStyle.addWalletButtonText}>
+              {t("Get Started")}
+            </Text>
+          </TouchableOpacity>
+        </ImageBackground>
+      ) : (
+        <View style={VaultScreenStyle.addWalletImage}>
+          <Video
+            source={require("../../assets/LightBg.mp4")}
+            style={StyleSheet.absoluteFill}
+            resizeMode="cover"
+            shouldPlay
+            isLooping
+            muted
+          />
+          <TouchableOpacity
+            //   onPress={handleContinue}
+            onPress={handleWalletTest} //      {/*  测试按钮test btn  */}
+            style={VaultScreenStyle.addWalletButton}
+          >
+            <Text style={VaultScreenStyle.addWalletButtonText}>
+              {t("Get Started")}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      )}
       <View style={VaultScreenStyle.walletInfoContainer}>
         <Text style={VaultScreenStyle.securityTitle}>
           {t("Security in your hands")}
