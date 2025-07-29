9499194ee97589f5157e6274c5370bde69d5fa80
Jaime Lee <18038163520@163.com>
2025-07-29 22:03:15 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 // App.js
 import { TextEncoder, TextDecoder } from "text-encoding";
 if (typeof global.TextEncoder === "undefined") global.TextEncoder = TextEncoder;
-if (typeof global.TextDecoder === "undefined") global.TextDecoder = TextDecoder;
 import "intl-pluralrules";
 import React, { useContext, useEffect, useState, useRef } from "react";
 import {
