9e0741c6140319d9beb15af0c349e3d9949adf02
Jaime Lee <18038163520@163.com>
2025-07-29 19:28:32 +0800

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -68,7 +68,7 @@ export default {
       },
     },
     supportsTablet: false,
-    buildNumber: "1.0.11",
+    buildNumber: "1.0.12",
     bundleIdentifier: "com.secnet.vaultapp",
     icon: "./assets/icon.png", //❌
   },
