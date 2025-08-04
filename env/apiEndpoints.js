78ceb129aa6a5081f77124f6c515a5492d156529
Jaime Lee <18038163520@163.com>
2025-08-04 10:10:23 +0800

    Update apiEndpoints.js

--- env/apiEndpoints.js
+++ env/apiEndpoints.js
@@ -1,25 +1,26 @@
 // env/apiEndpoints.js
 
 export const convertAPI = {
-  queryQuote: "https://swap.likkim.com/api/aggregator/queryQuote",
-  executeConvert: "https://swap.likkim.com/api/aggregator/swap",
+  queryQuote: "https://convert.lukkey.ch/api/aggregator/queryQuote",
+  executeConvert: "https://convert.lukkey.ch/api/aggregator/swap",
 };
 
 export const accountAPI = {
-  queryTransaction: "https://bt.likkim.com/api/wallet/queryTransaction",
-  blockchainFee: "https://bt.likkim.com/api/chain/blockchain-fee",
-  balance: "https://bt.likkim.com/api/wallet/balance",
-  broadcastHex: "https://bt.likkim.com/api/wallet/broadcastHex",
-  getSignParam: "https://bt.likkim.com/api/wallet/getSignParam",
-  encodeEvm: "https://bt.likkim.com/api/sign/encode_evm",
+  queryTransaction: "https://chain.lukkey.ch/api/wallet/queryTransaction",
+  blockchainFee: "https://chain.lukkey.ch/api/chain/blockchain-fee",
+  balance: "https://chain.lukkey.ch/api/wallet/balance",
+  broadcastHex: "https://chain.lukkey.ch/api/wallet/broadcastHex",
+  getSignParam: "https://chain.lukkey.ch/api/wallet/getSignParam",
+  encodeEvm: "https://chain.lukkey.ch/api/sign/encode_evm",
 };
 export const metricsAPII = {
   exchangeRate: "market.lukkey.ch/api/market/exchange-rate",
   indexTickers: "market.lukkey.ch/api/market/index-tickers",
 };
 export const galleryAPI = {
-  queryNFTBalance: "https://bt.likkim.com/api/nfts/query-address-balance-fills",
-  queryNFTDetails: "https://bt.likkim.com/api/nfts/query-nft-details",
+  queryNFTBalance:
+    "https://chain.lukkey.ch/api/nfts/query-address-balance-fills",
+  queryNFTDetails: "https://chain.lukkey.ch/api/nfts/query-nft-details",
 };
 
 export const externalLinks = {
@@ -28,7 +29,7 @@ export const externalLinks = {
 };
 
 export const meridianAPI = {
-  queryBlockList: "https://bt.likkim.com/meridian/address/queryBlockList",
+  queryBlockList: "https://chain.lukkey.ch/meridian/address/queryBlockList",
 };
 export const chartAPI = {
   indexCandles: "market.lukkey.ch/api/market/index-candles",
@@ -39,15 +40,15 @@ export const firmwareAPI = {
 };
 
 export const signAPI = {
-  encode_btc: "https://bt.likkim.com/api/sign/encode_btc",
-  encode_evm: "https://bt.likkim.com/api/sign/encode_evm",
-  encode_aptos: "https://bt.likkim.com/api/sign/encode_aptos",
-  encode_cosmos: "https://bt.likkim.com/api/sign/encode_cosmos",
-  encode_solana: "https://bt.likkim.com/api/sign/encode_solana",
-  encode_sui: "https://bt.likkim.com/api/sign/encode_sui",
-  encode_xrp: "https://bt.likkim.com/api/sign/encode_xrp",
-  aptos_broadcast: "https://bt.likkim.com/api/sign/aptos_broadcast",
-  cosmos_broadcast: "https://bt.likkim.com/api/sign/cosmos_broadcast",
-  solana_broadcast: "https://bt.likkim.com/api/sign/solana_broadcast",
-  sui_broadcast: "https://bt.likkim.com/api/sign/sui_broadcast",
+  encode_btc: "https://chain.lukkey.ch/api/sign/encode_btc",
+  encode_evm: "https://chain.lukkey.ch/api/sign/encode_evm",
+  encode_aptos: "https://chain.lukkey.ch/api/sign/encode_aptos",
+  encode_cosmos: "https://chain.lukkey.ch/api/sign/encode_cosmos",
+  encode_solana: "https://chain.lukkey.ch/api/sign/encode_solana",
+  encode_sui: "https://chain.lukkey.ch/api/sign/encode_sui",
+  encode_xrp: "https://chain.lukkey.ch/api/sign/encode_xrp",
+  aptos_broadcast: "https://chain.lukkey.ch/api/sign/aptos_broadcast",
+  cosmos_broadcast: "https://chain.lukkey.ch/api/sign/cosmos_broadcast",
+  solana_broadcast: "https://chain.lukkey.ch/api/sign/solana_broadcast",
+  sui_broadcast: "https://chain.lukkey.ch/api/sign/sui_broadcast",
 };
