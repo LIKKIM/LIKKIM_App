740b37fd1a86d3e67ce4d136a8a8cc0a38e94171
Jaime Lee <18038163520@163.com>
2025-08-15 00:50:14 +0800

    Update EmptyWalletView.js

--- components/modal/EmptyWalletView.js
+++ components/modal/EmptyWalletView.js
@@ -17,28 +17,6 @@ const EmptyWalletView = ({
 }) => {
   return (
     <View style={VaultScreenStyle.centeredContent}>
-      <View style={VaultScreenStyle.addWalletImage}>
-        <Video
-          source={
-            isDarkMode
-              ? require("../../assets/darkBg.mp4")
-              : require("../../assets/LightBg.mp4")
-          }
-          style={StyleSheet.absoluteFill}
-          resizeMode="cover"
-          shouldPlay
-          isLooping
-          muted
-        />
-        <TouchableOpacity
-          onPress={global.__DEV__ ? handleWalletTest : handleContinue}
-          style={VaultScreenStyle.addWalletButton}
-        >
-          <Text style={VaultScreenStyle.addWalletButtonText}>
-            {t("Get Started")}
-          </Text>
-        </TouchableOpacity>
-      </View>
       <View style={VaultScreenStyle.walletInfoContainer}>
         <Text style={VaultScreenStyle.securityTitle}>
           {t("Security in your hands")}
