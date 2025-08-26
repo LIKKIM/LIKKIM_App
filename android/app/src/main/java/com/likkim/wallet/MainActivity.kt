2b79c207d9d2742d12a3a02a02027e03fa2b03af
Jaime Lee <18038163520@163.com>
2025-08-26 12:05:05 +0800

    Rename package from com.likkim.wallet to com.secnet.vaultapp
    Updated the Android app's namespace, applicationId, manifest intent filter, and Kotlin package declarations to use 'com.secnet.vaultapp' instead of 'com.likkim.wallet'. Also updated the mavenGroupId in the react-native-locale-detector patch to match the new package name. This change is part of a rebranding or migration effort.

--- android/app/src/main/java/com/likkim/wallet/MainActivity.kt
+++ android/app/src/main/java/com/likkim/wallet/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.likkim.wallet
+package com.secnet.vaultapp
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/likkim/wallet/MainApplication.kt
+++ android/app/src/main/java/com/likkim/wallet/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.likkim.wallet
+package com.secnet.vaultapp
 
 import android.app.Application
 import android.content.res.Configuration
